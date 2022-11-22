7deb7e185fa7a9203e79be55a40cdce2399b42c7
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-11-22 16:02:28 +0500

    Update style

--- asist/js/x-0.js
+++ asist/js/x-0.js
@@ -28,8 +28,8 @@ function init() {
     board.appendChild(row);
     for (var j = 0; j < N_SIZE; j++) {
       var cell = document.createElement('td');
-      cell.setAttribute('height', 120);
-      cell.setAttribute('width', 120);
+      cell.setAttribute('height', 150);
+      cell.setAttribute('width', 150);
       cell.setAttribute('align', 'center');
       cell.setAttribute('valign', 'center');
       cell.classList.add('col' + j, 'row' + i);
